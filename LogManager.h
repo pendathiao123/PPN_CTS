3b0c91dd149cb7ecc836c36d04335451d97dbe5c
penda thiao <thiaopenda2503@gmail.com>
2024-10-28 00:41:42 +0100

    HISTORISATION DES TRANSACTION
    Dans ce commit j'ai ajouté les transactions, chaque transaction est caratérisée
    par un identifiant, un type(buy,sell), le nom de la crypto concernée, la quantité et le prix unitaire
    ainsi que de la date de la transaction.
    
    Toutes les transactions sont stocké dans un fichier log.csv.

---
+++ LogManager.h
@@ -0,0 +1,38 @@
+#include "Transaction.h"
+#include <fstream>    // Pour std::ofstream
+#include <iomanip>    // Pour formatage des dates
+#include <sstream>    // Pour convertir le timestamp
+#include <iostream>   // Pour std::cerr
+
+// Fonction pour formater un timestamp
+std::string formatTimestamp(std::time_t timestamp) {
+    std::tm* tmPtr = std::localtime(&timestamp);
+    std::ostringstream oss;
+    oss << std::put_time(tmPtr, "%Y-%m-%d %H:%M:%S");
+    return oss.str();
+}
+
+// Méthode pour ajouter une transaction au fichier CSV
+void addTransactionToCSV(const Transaction& transaction, const std::string& filename = "transactions.csv") {
+    std::ofstream file(filename, std::ios::app); // Ouvre le fichier en mode ajout
+    
+    if (file.is_open()) {
+        // Si c'est un fichier vide, ajoute l'en-tête
+        if (file.tellp() == 0) {
+            file << "ID,Type,CryptoName,Quantity,UnitPrice,TotalAmount,Timestamp\n";
+        }
+        
+        // Ecriture de la transaction
+        file << transaction.getId() << ","
+             << transaction.getType() << ","
+             << transaction.getCryptoName() << ","
+             << transaction.getQuantity() << ","
+             << transaction.getUnitPrice() << ","
+             << transaction.getTotalAmount() << ","
+             << formatTimestamp(transaction.getTimestamp()) << "\n";
+        
+        file.close();
+    } else {
+        std::cerr << "Erreur : Impossible d'ouvrir le fichier " << filename << " pour l'écriture.\n";
+    }
+}
--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
 #include "Server.h"
 #include "Client.h"
+#include "Transaction.h"
 
 int main() {
-    // Spécification de l'adresse IP et du port
-    std::string serverIp = "127.0.0.1";
-    int serverPort = 8080;
 
-    // Initialisation du serveur avec une adresse IP spécifique
-    Server server(serverIp, serverPort, "cryptos.json");
-    server.start();
-
-    // Création du client pour se connecter au serveur
-    Client client(serverIp, serverPort);
-    client.sendRequest("GET /market");
-
-    std::cout << "Réponse du serveur: " << client.receiveResponse() << std::endl;
+    Transaction tx("buy", "Bitcoin", 0.5, 30000.0);
+    Transaction::logTransactionToCSV(tx,"log.csv");
 
     return 0;
 }
