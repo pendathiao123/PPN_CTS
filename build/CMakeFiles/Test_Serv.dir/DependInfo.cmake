85aea5408ff5be3b2cc7de3e0c821327a9e87fcf
penda <thiaopenda2503@gmail.com>
2025-01-07 20:14:33 +0100

    Réajustement de la variation de la crypto pour avoir une varition au tour de 2% pour les données en seconde

---
+++ build/CMakeFiles/Test_Serv.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/penda/Documents/PPN/src/code/Bot.cpp" "CMakeFiles/Test_Serv.dir/src/code/Bot.cpp.o" "gcc" "CMakeFiles/Test_Serv.dir/src/code/Bot.cpp.o.d"
+  "/home/penda/Documents/PPN/src/code/Client.cpp" "CMakeFiles/Test_Serv.dir/src/code/Client.cpp.o" "gcc" "CMakeFiles/Test_Serv.dir/src/code/Client.cpp.o.d"
+  "/home/penda/Documents/PPN/src/code/Crypto.cpp" "CMakeFiles/Test_Serv.dir/src/code/Crypto.cpp.o" "gcc" "CMakeFiles/Test_Serv.dir/src/code/Crypto.cpp.o.d"
+  "/home/penda/Documents/PPN/src/code/Global.cpp" "CMakeFiles/Test_Serv.dir/src/code/Global.cpp.o" "gcc" "CMakeFiles/Test_Serv.dir/src/code/Global.cpp.o.d"
+  "/home/penda/Documents/PPN/src/code/Main_Serv.cpp" "CMakeFiles/Test_Serv.dir/src/code/Main_Serv.cpp.o" "gcc" "CMakeFiles/Test_Serv.dir/src/code/Main_Serv.cpp.o.d"
+  "/home/penda/Documents/PPN/src/code/SRD-BTC.cpp" "CMakeFiles/Test_Serv.dir/src/code/SRD-BTC.cpp.o" "gcc" "CMakeFiles/Test_Serv.dir/src/code/SRD-BTC.cpp.o.d"
+  "/home/penda/Documents/PPN/src/code/Server.cpp" "CMakeFiles/Test_Serv.dir/src/code/Server.cpp.o" "gcc" "CMakeFiles/Test_Serv.dir/src/code/Server.cpp.o.d"
+  "/home/penda/Documents/PPN/src/code/Transaction.cpp" "CMakeFiles/Test_Serv.dir/src/code/Transaction.cpp.o" "gcc" "CMakeFiles/Test_Serv.dir/src/code/Transaction.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
