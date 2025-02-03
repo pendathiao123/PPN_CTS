52f3f2c3ac8d01c3b0983aebddfa21cbf50c46fe
Estelle <estelle.oliveira@ens.uvsq.fr>
2025-02-03 16:15:19 +0100

    Added the logic to display the datas from csv file

--- src/code/app.py
+++ src/code/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import os
+import pandas as pand
 
 app = Flask(__name__)
 
@@ -10,9 +11,10 @@ app.template_folder = app.config['TEMPLATE_FOLDER']
 # Configuration fictive pour ASSETS_ROOT
 app.config['ASSETS_ROOT'] = '/static'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    table = pand.read_csv('../data/btc_sec_values.csv', encoding= 'unicode_escape')
+    return render_template('index.html', data=table.to_html())
 
 if __name__ == '__main__':
     app.run(debug=True)
