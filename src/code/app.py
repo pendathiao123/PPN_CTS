79948dd09164774e319b95b08edc5edf45ed2c77
Estelle <estelle.oliveira@ens.uvsq.fr>
2025-02-03 19:21:11 +0100

    Implemented graphic display feature and improve user interface
    Graph display added in Semi-Random Bitcoin to plot the values over the time, time is defined by the day and the second.
    Improved user interface by adding parameters to position boxes at chosen spots.

--- src/code/app.py
+++ src/code/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import os
 import pandas as pand
+import json
 
 app = Flask(__name__)
 
@@ -14,7 +15,16 @@ app.config['ASSETS_ROOT'] = '/static'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     table = pand.read_csv('../data/btc_sec_values.csv', encoding= 'unicode_escape')
-    return render_template('index.html', data=table.to_html())
+    #transactions = pand.read_csv('../../log.csv', encoding= 'unicode_escape') ne marche pas encore, on doit extraire chaque type de données indépendemment
+
+    # Combiner 'Day' et 'Second' pour créer la colonne 'Date' ainsi on aura 0 1 pour la seconde 1 a date du jour 0. 
+    table['Date'] = table['Day'].astype(str) + ' ' + table['Second'].astype(str)
+
+    dates = table['Date'].tolist()
+    values = table['Value'].tolist()
+
+    #on retourne le fichier html mais aussi les dates et valeurs pour plot le graph
+    return render_template('index.html', dates=dates, values=values)
 
 if __name__ == '__main__':
     app.run(debug=True)
