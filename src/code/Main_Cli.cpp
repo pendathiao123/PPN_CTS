d2fc02ca0cfa2086a548781b21b0ee4846a3ce97
Estelle <estelle.oliveira@ens.uvsq.fr>
2025-04-12 20:02:19 +0200

    Added Gauges for SRD-BTC plot and clients balance
    Modified Monotoring.cpp in order to add gauges for SRD-BTC plot and clients balance (DOLLARS and SRD-BTC).
    Balances and SRD-BTC price are update each second in prometheus.
    Added config.yml to configure prometheus server.

--- src/code/Main_Cli.cpp
+++ src/code/Main_Cli.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "../headers/Client.h"
 #include "../headers/Bot.h"
 
+Bot bot;
 
 int main() {
     Client client;
     client.StartClient("127.0.0.1", 4433, "7474", "77d7728205464e7791c58e510d613566874342c26413f970c45d7e2bc6dd9836");
     client.buy("SRD-BTC", 50);
+    while(true) {
+        double a = bot.getBalance("SRD-BTC");
+        double c = bot.getBalance("DOLLARS");
+        
+        std::cout << a << "SRD-BTC" << c << "DOLLARS" << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(120));
+    }
     return 0;
 }
--- src/code/Monitoring.cpp
+++ src/code/Monitoring.cpp
@@ -1,23 +1,55 @@
 #include "../headers/Monitoring.h"
 #include <prometheus/exposer.h>
 #include <prometheus/registry.h>
-#include <prometheus/counter.h>
+#include <prometheus/gauge.h>
 #include <chrono>
 #include <thread>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "../headers/Bot.h" //pour atteindre getBalance
+#include "../headers/Global.h" //pour lire fichier csv
 
 using namespace prometheus;
+Bot bot; //Nous permet d'accéder aux différentes fonctions (getPrice et get_total_Balance)
 
+/*Fonction qui lance la récupération des données utiles qui seront visibles à l'adresse 127.0.0.1:8080*/
 void PrometheusServer() {
     Exposer exposer{"127.0.0.1:8080"};
-    auto registry = std::make_shared<Registry>();
+    auto registry = std::make_shared<Registry>();   //création d'un registre ou on met les gauges etc.. 
 
-    auto& counter = BuildCounter()
-        .Name("Test")
+    //Balances clients
+    auto& balance_gauge = BuildGauge()
+        .Name("Balance_client")
+        .Help("Balances d'un client")
         .Register(*registry);
+
+    //SRD-BTC graphique
+    auto& SRDBTC_gauge = BuildGauge()
+        .Name("SRD_BTC")
+        .Help("Graphique du SRD-BTC au cours du temps")
+        .Register(*registry);
+    auto& SRD_BTC_gauge = SRDBTC_gauge.Add({}); //permet de définir une gauge sans Labels (pas FamilyGauge)
 
     exposer.RegisterCollectable(registry);
 
     while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(120));
+        std::unordered_map<std::string, double> balances = bot.get_total_Balance();
+
+        /*mise à jour du solde des portefeuilles clients du point de vue de Prometheus; récupère les données*/
+        for (const auto& [currency, balance] : balances) {
+            balance_gauge
+                .Add({{"currency", currency}})
+                .Set(balance);
+            std::cout << "Mise à jour du solde pour la devise " << currency << ": " << balance << std::endl;
+        }
+
+        /*Obtenir le prix du SRC-BTC*/
+        double prix_btc = bot.getPrice("SRD-BTC");
+        SRD_BTC_gauge.Set(prix_btc);
+        std::cout << "Mise à jour du prix du SRD-BTC : " << prix_btc << std::endl; 
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
