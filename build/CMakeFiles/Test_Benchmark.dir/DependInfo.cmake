1b6cef33105ac2401f562b62263ca5aca59d4cbc
Estelle <estelle.oliveira@ens.uvsq.fr>
2025-05-02 21:32:23 +0200

    Modified CMakeLists.txt

---
+++ build/CMakeFiles/Test_Benchmark.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/estelleoliveira/Programmation/PPN/PPN/PPN_CTS/src/code/Benchmark.cpp" "CMakeFiles/Test_Benchmark.dir/src/code/Benchmark.cpp.o" "gcc" "CMakeFiles/Test_Benchmark.dir/src/code/Benchmark.cpp.o.d"
+  "/home/estelleoliveira/Programmation/PPN/PPN/PPN_CTS/src/code/ClientInitiator.cpp" "CMakeFiles/Test_Benchmark.dir/src/code/ClientInitiator.cpp.o" "gcc" "CMakeFiles/Test_Benchmark.dir/src/code/ClientInitiator.cpp.o.d"
+  "/home/estelleoliveira/Programmation/PPN/PPN/PPN_CTS/src/code/Logger.cpp" "CMakeFiles/Test_Benchmark.dir/src/code/Logger.cpp.o" "gcc" "CMakeFiles/Test_Benchmark.dir/src/code/Logger.cpp.o.d"
+  "/home/estelleoliveira/Programmation/PPN/PPN/PPN_CTS/src/code/ServerConnection.cpp" "CMakeFiles/Test_Benchmark.dir/src/code/ServerConnection.cpp.o" "gcc" "CMakeFiles/Test_Benchmark.dir/src/code/ServerConnection.cpp.o.d"
+  "/home/estelleoliveira/Programmation/PPN/PPN/PPN_CTS/src/code/Transaction.cpp" "CMakeFiles/Test_Benchmark.dir/src/code/Transaction.cpp.o" "gcc" "CMakeFiles/Test_Benchmark.dir/src/code/Transaction.cpp.o.d"
+  "" "Test_Benchmark" "gcc" "CMakeFiles/Test_Benchmark.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Test_Benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test_Benchmark.dir/link.d"
+  "CMakeFiles/Test_Benchmark.dir/src/code/Benchmark.cpp.o"
+  "CMakeFiles/Test_Benchmark.dir/src/code/Benchmark.cpp.o.d"
+  "CMakeFiles/Test_Benchmark.dir/src/code/ClientInitiator.cpp.o"
+  "CMakeFiles/Test_Benchmark.dir/src/code/ClientInitiator.cpp.o.d"
+  "CMakeFiles/Test_Benchmark.dir/src/code/Logger.cpp.o"
+  "CMakeFiles/Test_Benchmark.dir/src/code/Logger.cpp.o.d"
+  "CMakeFiles/Test_Benchmark.dir/src/code/ServerConnection.cpp.o"
+  "CMakeFiles/Test_Benchmark.dir/src/code/ServerConnection.cpp.o.d"
+  "CMakeFiles/Test_Benchmark.dir/src/code/Transaction.cpp.o"
+  "CMakeFiles/Test_Benchmark.dir/src/code/Transaction.cpp.o.d"
+  "Test_Benchmark"
+  "Test_Benchmark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test_Benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
